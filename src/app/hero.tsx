a7d0b30a8c7d7048db9c2da97ba32daab33429d7
Raul <ra.do.nu20@gmail.com>
2025-03-21 22:05:21 -0400

    crackdevops

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -10,18 +10,17 @@ function Hero() {
       <header className="grid !min-h-[49rem] bg-gray-900 px-8">
         <div className="container mx-auto  grid h-full w-full grid-cols-1 place-items-center lg:mt-14 lg:grid-cols-3">
           <div className="col-span-2 ">
-            <Typography variant="h1" color="white" className="mb-4 bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-orange-500">
+            <h1 color="white" className="mb-4 bg-gradient-to-r from-red-600 to-orange-600">
               Innovación, Excelencia y Control Total de tu Infraestructura
-            </Typography>
-            <Typography
-              variant="lead"
+            </h1>
+            <p
               className="mb-16 !text-white md:pr-16 xl:pr-28"
             >
               Bienvenido a CrackDevOps, tu socio estratégico en consultoría avanzada 
               de ingeniería de plataformas y DevOps. En un mundo digital que evoluciona 
               rápidamente, no puedes darte el lujo de externalizar tu ventaja competitiva. 
               Nuestro enfoque único combina Platform Engineering, DevOps, Inteligencia Artificial (IA), y metodologías ágiles para asegurar que tu infraestructura tecnológica sea tu mejor activo.
-            </Typography> 
+            </p> 
           </div>
           <Lanyard  position={[0, 0, 20]} gravity={[0, -40, 0]} />
           
